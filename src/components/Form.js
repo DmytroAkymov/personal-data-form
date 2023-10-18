5c12c662dc0861a53321b9135347208b2d800f76
Dmytro <2dmitri.akiomv@gmail.com>
2023-10-18 12:05:49 +0200

    Form-module.css

---
+++ src/components/Form.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Form.module.css';
+
+const Form = () => {
+    return (
+        <div className={styles['user-form']}>
+            <h1>Enter user details:</h1>
+            <div className={styles['form-input']}>
+                <labe className={styles['input-label']}>Name:</labe>
+                <input type="text" id="name" className={styles['name-input']} />
+            </div>
+            <div className={styles['form-input']}>
+                <labe className={styles['input-label']}>Age:</labe>
+                <input type="number" id="age" className={styles['age-input']} />
+            </div>
+            <button className={styles['create-user']} type="submit">
+                Create a user
+            </button>
+        </div>
+    );
+};
+
+export default Form;
